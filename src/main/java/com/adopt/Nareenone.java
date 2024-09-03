def3b00d209730b439d5693a68e9a4a76b316ca3
unknown <rktosca113@gmail.com>
2024-09-03 11:15:38 +0530

    added new class

---
+++ src/main/java/com/adopt/Nareenone.java
@@ -0,0 +1,7 @@
+package com.adopt;
+
+public class Nareenone {
+    public static void main(String[]args){
+        System.out.println("i am ravi");
+    }
+}
---
+++ src/main/java/com/adopt/Polution.java
@@ -0,0 +1,4 @@
+package com.adopt;
+
+public class Polution                           {
+}
